6fcdd3c94c42c5c284a9973175bb289d68b63608
Nikoloz97 <nick.gotsy@gmail.com>
2023-01-19 07:54:09 -0500

    Add project files.

---
+++ ScratchPad/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+// Allows you to create lists/arrays
+using System.Collections.Generic;
+// Allows you to use Console.ReadLine()
+using System.IO;
+// Allows you to use query statements (like in SQL)
+using System.Linq;
+
+namespace ScratchPad
+{
+     class Program
+    {
+        string color;
+        int maxSpeed;
+        public string FullThrottle()
+        {
+            return "This car is going super fast!";
+        }
+        
+         static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.color = "red";
+            myProgram.maxSpeed = 200;
+
+            Console.WriteLine(myProgram.FullThrottle());
+        }
+
+
+
+
+
+
+
+
+
+
+        // Fizzbuzz practice...
+
+     /*   public static void FizzBuzzFunction(int valueInput)
+        {
+            if (valueInput % 3 == 0 && valueInput % 5 == 0) {
+                Console.WriteLine("FizzBuzz");
+            }
+
+            else if (valueInput % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            else if (valueInput % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else
+            {
+                Console.WriteLine(valueInput);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                FizzBuzzFunction(i);
+            }
+        }*/
+
+
+
+        }
+
+}
+
---
+++ ScratchPad/Recursion.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScratchPad
+{
+    public class Recursion
+    {
+        public int Factorial(int number)
+        {
+            if (number == 1) { return 1; }
+            return number * Factorial(number - 1);
+        }
+
+
+        // factorial 5 = return (5 x factorial(4))
+        // -> factorial(4) = return (4 x factorial(3))
+        // --> factorial(3) = return (3 x factorial(2));
+        // ---> factorial(2) = return (2 x factorial(1));
+
+        // ----> factorial(1) = return 1
+
+        // Now it reverses back up...
+
+        // ---> factorial(2) = return (2 x 1) = 2;
+        // --> facorial(3) = return (3 x 2) = 6;
+        // -> factorial(4) = return (4 x 6) = 24;
+        // factorial 5 = return 5 x 24 = 120;
+
+
+        public string ReverseWord(string Word)
+        {
+            if (Word.Length == 0) { return "";}
+            return ReverseWord(Word.Substring(1)) + Word[0];
+        }
+
+        // ReverseWord("Hello") = return ReverseWord(ello) + H;
+        // -> ReverseWord(ello) = return ReverseWord(llo) + e;
+        // --> ReverseWord(llo) = return ReverseWord(lo) + l;
+        // ---> ReverseWord(lo) = return ReverseWord(o) + l;
+        // ----> ReverseWord(o) = return ReverseWord() + o;
+        // -----> ReverseWord() = return "";
+
+        // Now it reverses back up...
+
+        // ----> ReverseWord(o) = return o;
+        // ---> ReverseWord(lo) = return o + l;
+        // --> ReverseWord(llo) = return ol + l;
+        // -> ReverseWord(ello) = oll + e;
+        // ReverseWord("Hello") = olle + H;
+
+        public int CountDown(int Value)
+        {
+
+            if (Value == 0) 
+            {
+                Console.WriteLine(Value);
+                Console.WriteLine("Buhya");
+                return 0;
+            }
+            Console.WriteLine(Value);
+            return CountDown(Value - 1) + 1;
+            
+        }
+
+        // Countdown(3) = 2 + 1; -> 3
+        // Countdown(2) = 1 + 1; -> 2
+        // Countdown(1) = 0 + 1; -> 1
+        // Countdown(0) = 0;  -> 0 + Buhya
+
+
+        public int SumRange(int Value)
+        {
+            if (Value == 0)
+            {
+                return 0;
+            }
+            return Value + SumRange(Value - 1);
+        }
+   
+
+
+
+    }
+}
