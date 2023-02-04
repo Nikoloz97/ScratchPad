171c8c7387e428be76ef69c3959d031c6989eb50
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-04 15:04:15 -0500

    Created DefangAddress function

---
+++ ScratchPad/LeetCode/Defang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScratchPad.LeetCode
+{
+    // Link: https://leetcode.com/problems/defanging-an-ip-address/
+    public class Defang
+    {
+
+        public string DefangIPaddress(string address)
+        {
+           
+            return address.Replace(".", "[.]");
+            
+        }
+    }
+}
