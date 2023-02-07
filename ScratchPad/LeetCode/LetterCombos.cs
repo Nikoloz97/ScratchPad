f53f231acd815b804a30f11408aba117ce9156eb
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-07 09:51:35 -0500

    Fixed lettercombos class

--- ScratchPad/LeetCode/LetterCombos.cs
+++ ScratchPad/LeetCode/LetterCombos.cs
@@ -10,73 +10,183 @@ namespace ScratchPad.LeetCode
     public class LetterCombos
     {
         // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+        // Constraint = digits.Length = 0 - 4 
+
+        Dictionary<char, string> phoneButtons = new Dictionary<char, string>()
+        {
+            {'2', "abc" },
+            {'3', "def" },
+            {'4', "ghi" },
+            {'5', "jkl" },
+            {'6', "mno" },
+            {'7', "pqrs" },
+            {'8', "tuv" },
+            {'9', "wxyz" }
+
+        };
+
         public IList<string> GetLetterCombinations(string digits) 
         {
-            // E.g. 2, 4, 6
-            // Create dictionary
-            // Create string array
-            // Create empty list
-
-            // For each digit (key) in string...
-                // Add it to a string array
-
-            // For each item in string array (i)...
-                // Loop through each char of string variable (j)... 
-                        // Each time, adding string[j] of each of the rest of items in string array to a "combination" string
-                // Once combination string is as long as digits, add to list
-           
-            // Return list
-
-
-            var phoneButtons = new Dictionary<int, string>() {
-                {'2', "abc" },
-                {'3', "def" },
-                {'4', "ghi" },
-                {'5', "jkl" },
-                {'6', "mno" },
-                {'7', "pqrs" },
-                {'8', "tuv" },
-                {'9', "wxyz" }
-            };
-
             IList<string> combinations = new List<string>();
 
-            // For each of the characters in digits... 
-            for (int i = 0; i < digits.Length; i++)
+            switch (digits.Length)
             {
-                // Find matching string value (Linq)
-                IEnumerable<string> enumButtonChars = from keyValue in phoneButtons where keyValue.Key == digits[i] select keyValue.Value;
-                string buttonChars = enumButtonChars.ToString();
-
-                // For each item of that string...
-                for (int j = 0; j < buttonChars.Length; j++)
-                {
-                    // Create combination string (starting with that item)
-                    string combination = string.Empty;
-                    combination += buttonChars[j];
-
-                    // For each of the next characters in digits...
-                    for (int k = i; k < digits.Length - i; k++)
-                    {
-                        // Find matching string value (Linq)
-                        IEnumerable<string> nextEnumButtonChars = from keyValue in phoneButtons where keyValue.Key == digits[i] select keyValue.Value;
-                        string nextButtonChars = nextEnumButtonChars.ToString();
-
-                        // Add to combination string
-                        combination += buttonChars[k];
-
-
-                    }
-
-                    // Add combination to list
-                    combinations.Add(combination);
-
-                }
-                
-           
+                case 0:
+                    break;
+                case 1:
+                    return CombListCreator_1(digits);
+                case 2:
+                    return CombListCreator_2(digits);
+                case 3:
+                    return CombListCreator_3(digits);
+                case 4:
+                    return CombListCreator_4(digits);
             }
 
             return combinations;
+
+        }
+
+
+
+
+        public IList<String> CombListCreator_1(string digits)
+        {
+            IList<string> combinationsList = new List<string>();
+
+
+            IEnumerable<string> enumButtonChars = from keyValue in phoneButtons where keyValue.Key == digits[0] select keyValue.Value;
+            string buttonChars = enumButtonChars.ToString();
+
+            foreach (var item in buttonChars)
+            {
+                combinationsList.Add(item.ToString());
+
+            }
+            return combinationsList;
+
+        }
+
+        public IList<String> CombListCreator_2(string digits)
+        {
+            IList<string> combinationsList = new List<string>();
+
+            IEnumerable<string> enumButtonChars_1 = from keyValue in phoneButtons where keyValue.Key == digits[0] select keyValue.Value;
+            string buttonChars_1 = enumButtonChars_1.ToString();
+
+            IEnumerable<string> enumButtonChars_2 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_2 = enumButtonChars_2.ToString();
+
+            string combination = string.Empty;
+
+            foreach (var item1 in buttonChars_1)
+            {
+                combination += item1;
+
+                foreach (var item2 in buttonChars_2)
+                {
+                    combination += item2;
+
+                    combinationsList.Add(combination);
+
+                    combination.Remove(1);
+                }
+
+                combination.Remove(0);
+            }
+
+            return combinationsList;
+
+        }
+
+        public IList<String> CombListCreator_3(string digits)
+        {
+            IList<string> combinationsList = new List<string>();
+
+            IEnumerable<string> enumButtonChars_1 = from keyValue in phoneButtons where keyValue.Key == digits[0] select keyValue.Value;
+            string buttonChars_1 = enumButtonChars_1.ToString();
+
+            IEnumerable<string> enumButtonChars_2 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_2 = enumButtonChars_2.ToString();
+
+            IEnumerable<string> enumButtonChars_3 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_3 = enumButtonChars_3.ToString();
+
+
+            string combination = string.Empty;
+
+            foreach (var item1 in buttonChars_1)
+            {
+                combination += item1;
+
+                foreach (var item2 in buttonChars_2)
+                {
+                    combination += item2;
+
+                    foreach (var item3 in buttonChars_3)
+                    {
+                        combination += item3;
+                        combinationsList.Add(combination);
+                        combination.Remove(2);
+                    }
+
+                    combination.Remove(1);
+                }
+
+                combination.Remove(0);
+            }
+
+            return combinationsList;
+        }
+
+        public IList<String> CombListCreator_4(string digits)
+        {
+            IList<string> combinationsList = new List<string>();
+
+            IEnumerable<string> enumButtonChars_1 = from keyValue in phoneButtons where keyValue.Key == digits[0] select keyValue.Value;
+            string buttonChars_1 = enumButtonChars_1.ToString();
+
+            IEnumerable<string> enumButtonChars_2 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_2 = enumButtonChars_2.ToString();
+
+            IEnumerable<string> enumButtonChars_3 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_3 = enumButtonChars_3.ToString();
+
+            IEnumerable<string> enumButtonChars_4 = from keyValue in phoneButtons where keyValue.Key == digits[1] select keyValue.Value;
+            string buttonChars_4 = enumButtonChars_4.ToString();
+
+            string combination = string.Empty;
+
+
+
+            foreach (var item1 in buttonChars_1)
+            {
+                combination += item1;
+
+                foreach (var item2 in buttonChars_2)
+                {
+                    combination += item2;
+
+                    foreach (var item3 in buttonChars_3)
+                    {
+                        combination += item3;
+
+                        foreach (var item4 in buttonChars_3)
+                        {
+                            combination += item4;
+                            combinationsList.Add(combination);
+
+                        }
+                        combination.Remove(2);
+                    }
+
+                    combination.Remove(1);
+                }
+
+                combination.Remove(0);
+            }
+
+            return combinationsList;
 
         }
     }
