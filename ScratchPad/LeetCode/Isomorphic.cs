3f60aa6ec1c7e553a3c28357727c2fdc1c71be11
Nikoloz97 <nick.gotsy@gmail.com>
2023-02-17 15:55:13 -0500

    refactored IsIsomorphic function

--- ScratchPad/LeetCode/Isomorphic.cs
+++ ScratchPad/LeetCode/Isomorphic.cs
@@ -9,69 +9,46 @@ namespace ScratchPad.LeetCode
     {
         public bool IsIsomorphic(string s, string t)
         {
-            // Create s index (int) list 
-            // Create t index (int) list
-
-
-            //Loop through s (i), starting from counter + 1
-            // if s(i) = s(counter), add i to s-list
-
-            //Loop through t (i), starting from counter + 1
-            // if t(i) = t(counter), add i to t-list
-
-            //Loop through s-array (i)
-            // If s-list(i) != t-list (i), isomorphicChecker == false
-
-            // Counter++ 
-
-            bool isomorphicChecker = true;
-
-            int counter = 0;
+            // s = "paper"
+            // t = "title"
 
             List<int> sIndexList = new List<int>();
             List<int> tIndexList = new List<int>();
 
-            while (isomorphicChecker && counter < s.Length)
+            int counter = 0;
+
+            while (counter < s.Length)
             {
-                for (int i = counter; i < s.Length; i++)
+                for (int i = counter + 1; i < s.Length; i++)
                 {
                     if (s[i] == s[counter])
                     {
                         sIndexList.Add(i);
                     }
 
-                }
-
-                for (int i = counter; i < t.Length; i++)
-                {
                     if (t[i] == t[counter])
                     {
                         tIndexList.Add(i);
                     }
 
-                }
-
-                if (tIndexList.Count != sIndexList.Count)
-                {
-                    isomorphicChecker = false;
-                    return isomorphicChecker;
-                }
-
-                for (int j = 0; j < sIndexList.Count; j++)
-                {
-                    if (sIndexList[j] != tIndexList[j])
+                    if (tIndexList.Count != sIndexList.Count)
                     {
-                        isomorphicChecker = false;
-                        return isomorphicChecker;
-
+                        return false;
                     }
 
-                }
+                    for (int j = 0; j < sIndexList.Count; j++)
+                    {
+                        if (sIndexList[j] != tIndexList[j])
+                        {
+                            return false;
 
+                        }
+                    }
+                }
                 counter++;
             }
 
-            return isomorphicChecker; 
+            return true; 
         }
     }
 }
