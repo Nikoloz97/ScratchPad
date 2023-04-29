ea15d96f08eed48eeee90401892baca736e8da3c
Nikoloz97 <nick.gotsy@gmail.com>
2023-04-29 11:31:36 -0400

    Creeated draft for MinNumOfHours function

---
+++ ScratchPad/LeetCode/Competition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScratchPad.LeetCode
+{
+    // https://leetcode.com/problems/minimum-hours-of-training-to-win-a-competition/
+    public class Competition
+    {
+        public int MinNumOfHours (int initialEnergy, int initialExperience, int[] energy, int[] experience )
+        {
+            int trainingHours = 0;
+            int opponent = 0;
+
+            // While opponent <= energy.Length - 1
+            // for item in energy array
+            // If energy[i] and experience[i] is greater than iEn and iEx, increase experience and decrease energy
+            // Else, while energy is less and experience is less...
+            // If energy is less, increment tH, iEn by 1;  else-if experience is low, increment tH, iEx by 1
+
+            while (opponent <= energy.Length - 1)
+            {
+                opponent++;
+                for (int i = 0; i <= energy.Length; i++)
+                {
+                    if (energy[i] <= initialEnergy || experience[i] <= initialExperience)
+                    {
+                        initialExperience += experience[i];
+                        initialEnergy -= energy[i];
+                    }
+                    else
+                    {
+                        while (energy[i] > initialEnergy || experience[i] > initialExperience)
+                        {
+                            trainingHours++;
+
+                            if (energy[i] > initialEnergy) { initialEnergy++; }
+
+                            else { initialExperience++; }
+                        }
+                    }
+                }
+                
+            }
+
+            return trainingHours;
+
+        }
+    }
+}
---
+++ ScratchPad/LeetCode/MergeArrays.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScratchPad.LeetCode
+{
+    public class MergeArrays
+    {
+        public int[][] MergeArraysFunction(int[][] nums1, int[][] nums2)
+        {
+            List<int[]> mergedList = nums1.ToList<int[]>();
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (nums1[i][0] == nums2[i][0])
+                {
+                    mergedList[i][1] += nums2[i][1];
+                }
+
+                else if (nums1[i][0] > nums2[i][0])
+                {
+                    while (nums1[i][0] > nums2[i][0])
+                    {
+                        int j = i;
+                        j++;
+                        if (nums1[i][0] == nums2[j][0])
+                        {
+                            mergedList[i][1] += nums2[i][1];
+                        }
+                    }
+                }
+
+                else
+                {
+                    mergedList.Add(nums2[i]);
+                }
+
+            }
+
+            return mergedList.ToArray();
+        }
+    }
+}
